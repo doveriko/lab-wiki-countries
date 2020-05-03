e9191a9c2b74f48e516f2236ee922483e389e695
doveriko <abelmorpe@gmail.com>
2020-05-03 19:07:28 +0200

    lab redone completely

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import countries from "./countries.json";
+import CountryDetail from "./CountryDetail";
+import { Link, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            WikiCountries
+          </a>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-5" style={{ maxHeight: '90vh', overflow: 'scroll' }}>
+            <div className="list-group">
+              {countries.map(country => (
+                <Link
+                  key={country.cca3}
+                  className="list-group-item list-group-item-action"
+                  to={`/${country.cca3}`}
+                >
+                  <span>{country.flag} </span>{country.name.common}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <Switch>
+            <Route path="/:cca3" render={ props =>
+            (<CountryDetail {...props} countries={countries} />)
+            } />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
